25ceca6fc6dd4bd725f8156f4d7d767bc12649ba
Brian Anglin <brianranglin+gh@gmail.com>
2017-10-15 16:58:40 -0700

    adds green colors

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,9 @@ const globalConfig = {
   darkText: '#676E6E',
   lightText: '#fff',
   font: 'Avenir Next',
-  greenGradient: ['#06AEC1', '#06BFA7']
+  greenGradient: ['#06AEC1', '#06BFA7'],
+  lightGreen: '#70D1C4',
+  darkerGreen: '#64C2B6',
 }
 
 const development = {
